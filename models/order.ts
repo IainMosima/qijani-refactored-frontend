d3159b65d6f38263074146b715b9985bbae7b5ad
IainMosima <iainmosima@gmail.com>
2023-09-02 20:45:41 +0300

    fixing build error

--- models/order.ts
+++ models/order.ts
@@ -11,6 +11,6 @@ export interface newOrder {
     userId: string,
     price: string,
     packageId: string,
-    paymentStatus: string,
-    delivered: string,
+    paymentType: string,
+    phoneNumber: string,
 }
