320544e13b7805d514596b80421ab901e068ab5e
IainMosima <iainmosima@gmail.com>
2023-09-11 17:10:25 +0300

    working on user logging

--- components/LoginSignUp/Login.tsx
+++ components/LoginSignUp/Login.tsx
@@ -37,10 +37,8 @@ const LoginForm = ({ setErrorText }: LoginProps) => {
   async function onSubmit(credentials: loginCredentials) {
     try {
       const token = await login({...credentials, usernameEmail: credentials.usernameEmail.toLowerCase()});
-      console.log(token);
       localStorage.setItem('token', token);
       const user = await getLoggedInUser(token);
-      console.log(user);
       
       if (user) {
         dispatch(userLogin(user));
