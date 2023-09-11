ffd026838dde0a79a5bf725b3b0279c575fa87a7
IainMosima <iainmosima@gmail.com>
2023-09-11 11:14:15 +0300

    fixing build errors

--- components/Packages/AddNewPackageModal/AddNewPackageModal.tsx
+++ components/Packages/AddNewPackageModal/AddNewPackageModal.tsx
@@ -17,40 +17,42 @@ interface AddNewPackageModalProps {
     loggedInUser: User
 }
 
-const AddNewPackageModal = ({open ,onClose, myPackages, loggedInUser } : AddNewPackageModalProps) => {
+const token = localStorage.getItem('token');
+const AddNewPackageModal = ({ open, onClose, myPackages, loggedInUser }: AddNewPackageModalProps) => {
     const { register, handleSubmit } = useForm();
     const dispatch = useAppDispatch();
-    async function onSubmit(data: Record<string, any>){
+    async function onSubmit(data: Record<string, any>) {
         const packageName = data.packageName;
+        let newPackages;
         try {
-          const newPackages = await createNewPackage({userId: loggedInUser._id, packageName: packageName});
-          dispatch(setMypackages([...myPackages, {_id: newPackages._id, userId: loggedInUser._id, packageName: packageName, items: packageName.items}]));
+            if (token) newPackages = await createNewPackage({ userId: loggedInUser._id, packageName: packageName }, token);
+            if (newPackages) dispatch(setMypackages([...myPackages, { _id: newPackages._id, userId: loggedInUser._id, packageName: packageName, items: packageName.items }]));
             onClose();
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      }
-    
-    return ( 
+    }
+
+    return (
         <Dialog
-         open={open}
-         onClose={onClose}
+            open={open}
+            onClose={onClose}
         >
             <div className="app__AddpackageModal">
                 <div className="title">
                     <h2 className="text-md">Add New package</h2>
-                    <Image src={Images.closeIcon} alt='close-icon' onClick={onClose}/>
+                    <Image src={Images.closeIcon} alt='close-icon' onClick={onClose} />
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div  className="field">
-                        <input type='text' placeholder="Enter Package Name" autoComplete="off" {...register('packageName')} className="border border-black rounded-sm "/>
+                    <div className="field">
+                        <input type='text' placeholder="Enter Package Name" autoComplete="off" {...register('packageName')} className="border border-black rounded-sm " />
 
                         <button>Add</button>
                     </div>
                 </form>
             </div>
         </Dialog>
-     );
+    );
 }
- 
+
 export default AddNewPackageModal;
