ea0f35a9f2cc75c5f0280d4a8c1c49b4e38920d8
Maina-nganga <ngangamaina091@gmail.com>
2025-04-22 23:14:40 +0300

    Added quiz form with validation.

---
+++ pages/quiz.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import { UserProfileQuiz } from '../components';
+
+const QuizPage = () => {
+  return (
+    <Container>
+      <UserProfileQuiz />
+    </Container>
+  );
+};
+
+export default QuizPage; 
